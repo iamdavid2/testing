207e4d9d10dd2f7675dcc4a04295c4a5a0a59be1
David Herrera <miemailesdavid@gmail.com>
2015-10-22 14:49:48 +0000

    first commit

---
+++ workshop/www/js/HomeView.js
@@ -0,0 +1,31 @@
+var HomeView = function (service) {
+
+    var sessionListView;
+
+    this.initialize = function () {
+        // Define a div wrapper for the view (used to attach events)
+        this.$el = $('<div/>');
+        this.$el.on('keyup', '.search-key', this.findByName);
+        sessionListView = new SessionListView();
+        this.render();
+    };
+
+
+    /* ---------------------------------- Local Functions ---------------------------------- */
+
+    this.render = function() {
+        this.$el.html(this.template());
+        $('.content', this.$el).html(sessionListView.$el);
+        return this;
+    };
+ 
+  
+    this.findByName = function() {
+        service.findByName($('.search-key').val()).done(function(sessions) {
+            sessionListView.setSessions(sessions);
+        });
+    };   
+    
+       
+    this.initialize();     
+}
